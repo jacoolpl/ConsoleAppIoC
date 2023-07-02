91a7ce22fb6c37378a161755aa622963e01c4590
Ja`cOol <jacool@jacool.pl>
2023-07-02 13:34:53 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+public interface IAutomobile
+{
+    void Ignition();
+    void Stop();
+}
+
+public class Jeep : IAutomobile
+{
+    #region IAutomobile Members
+    public void Ignition()
+    {
+        Console.WriteLine("Jeep start");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("Jeep stopped.");
+    }
+    #endregion
+}
+
+public class SUV : IAutomobile
+{
+    #region IAutomobile Members
+    public void Ignition()
+    {
+        Console.WriteLine("SUV start");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine("SUV stopped.");
+    }
+    #endregion
+}
+
+
+public class AutomobileController
+{
+    IAutomobile m_Automobile;
+
+    public AutomobileController(IAutomobile automobile)
+    {
+        this.m_Automobile = automobile;
+    }
+
+    public void Ignition()
+    {
+        m_Automobile.Ignition();
+    }
+
+    public void Stop()
+    {
+        m_Automobile.Stop();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IAutomobile automobile = new Jeep();
+        //IAutomobile automobile = new SUV();
+        AutomobileController automobileController = new AutomobileController(automobile);
+        automobileController.Ignition();
+        automobileController.Stop();
+
+        Console.Read();
+    }
+}
